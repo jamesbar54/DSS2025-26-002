180454fbe8a91eba79a51afe10817f72bb0d1dbb
abzozovskij <abzozovskij@gmail.com>
2026-05-14 11:36:34 +0100

    changed rate limiting error message

--- app/app.js
+++ app/app.js
@@ -16,7 +16,7 @@ const limiter = rateLimit({
 	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
-	message: {error: 'Too many requests, please try again later.'}
+	message: {error: 'Too many requests, please try again in a few minutes'}
 })
 
 
